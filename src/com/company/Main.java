1e3c193056bbc9319b3c7cc52f2b9e441d69d2b6
Bob <bjdhjy689@gmail.com>
2021-06-18 08:15:20 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
