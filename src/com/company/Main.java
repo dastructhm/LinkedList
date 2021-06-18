81ebd35b95ada698297783033ab27857b9aae401
Bob <bjdhjy689@gmail.com>
2021-06-18 08:46:17 +0800

    111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
+// https://en.wikipedia.org/wiki/Linked_list
+// https://www.runoob.com/java/java-linkedlist.html
+// https://stackoverflow.com/questions/322715/when-to-use-linkedlist-over-arraylist-in-java#:~:text=LinkedList%20is%20fast%20for%20adding,depth%2C%20as%20does%20Linked%20List
+// LinkedList is faster in add and remove, but slower in get. In brief, LinkedList should be preferred if:
+// 1. There are no large number of random access of element
+// 2. There are a large number of add/remove operations
+
 package com.company;
 import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<String> cars = new LinkedList<>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
+        LinkedList<String> product = new LinkedList<>();
+        product.add("Volvo");
+        product.add("BMW");
+        product.add("Ford");
+        product.add("Mazda");
+        System.out.println(product);
     }
 }
